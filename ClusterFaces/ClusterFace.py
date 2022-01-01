82c01addea10f03c126bdae3adf63d257f6e98d1
roya-beikloo <78058369+roya-beikloo@users.noreply.github.com>
2022-01-01 23:49:34 +0330

    add requirements file

--- ClusterFaces/ClusterFace.py
+++ ClusterFaces/ClusterFace.py
@@ -10,33 +10,27 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-
-
 class ClusterFace():
-    def __init__(self, dir_address='./', radius=0.2 ):
-        self.extensions  = ['jpg','png','jpeg']
+    def __init__(self, dir_address='./', radius=0.2):
+        self.extensions = ['jpg', 'png', 'jpeg']
         self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
         self.mtcnn = MTCNN(image_size=160)
         self.dir_address = dir_address
-        self.radius      = radius
+        self.radius = radius
         self.images_address = self.get_image_names(self.dir_address)
         self.data = self.get_embedding_face(self.images_address)
-    
-
 
     def get_image_names(self, address):
         files = []
         for extension in self.extensions:
-            files.extend(glob.glob(os.path.join(self.dir_address,f'*.{extension}')))
+            files.extend(glob.glob(os.path.join(self.dir_address, f'*.{extension}')))
         return files
-        
 
-
-    def cosine_similarity(self,vec1,vec2):
-        cos_sim = 1- (dot(vec1, vec2)/(norm(vec1)*norm(vec2)))
+    def cosine_similarity(self, vec1, vec2):
+        cos_sim = 1 - (dot(vec1, vec2) / (norm(vec1) * norm(vec2)))
         return cos_sim
-   
-    def get_embedding_face(self, images_address:str):
+
+    def get_embedding_face(self, images_address: str):
         data = []
         for image_address in images_address:
             img1 = cv2.imread(image_address)
@@ -44,11 +38,9 @@ class ClusterFace():
             img_cropped1 = self.mtcnn(img1)
             img_embedding1 = self.resnet(img_cropped1.unsqueeze(0))
             a = img_embedding1[0].cpu().detach().numpy()
-            data.append(img_embedding1[0].cpu().detach().numpy() )
+            data.append(img_embedding1[0].cpu().detach().numpy())
             # data.append(list(a))
         return data
-
-
 
     def fit(self):
         centroids = {}
@@ -56,7 +48,7 @@ class ClusterFace():
 
         for i in range(len(data)):
             centroids[i] = data[i]
-        
+
         while True:
             new_centroids = []
             new_centroids_data = {}
@@ -64,12 +56,12 @@ class ClusterFace():
                 in_bandwidth = []
                 in_bandwidth_idx = []
                 centroid = centroids[i]
-                for idx, featureset in enumerate(data):
-                    if self.cosine_similarity(featureset,centroid) < self.radius:
-                        in_bandwidth.append(featureset)
+                for idx, feature_set in enumerate(data):
+                    if self.cosine_similarity(feature_set, centroid) < self.radius:
+                        in_bandwidth.append(feature_set)
                         in_bandwidth_idx.append(idx)
 
-                new_centroid = np.average(in_bandwidth,axis=0)
+                new_centroid = np.average(in_bandwidth, axis=0)
                 new_centroids.append(tuple(new_centroid))
                 new_centroids_data[i] = in_bandwidth_idx
 
@@ -88,39 +80,34 @@ class ClusterFace():
                     optimized = False
                 if not optimized:
                     break
-                
+
             if optimized:
                 break
 
-        self.centroids          = centroids
+        self.centroids = centroids
         self.new_centroids_data = new_centroids_data
 
     def get_max_class(self):
-        return max(len(v) for k,v in self.new_centroids_data.items())
+        return max(len(v) for k, v in self.new_centroids_data.items())
 
     def show_results(self):
-        print (f'we have {len(self.centroids)} persons in the images as follow:')
-        for idx in range (len(self.centroids)):
-            print ( f'for person {idx}:')
-            for j in range (len((self.new_centroids_data[idx]))):
-                print (self.images_address[self.new_centroids_data[idx][j]])
+        print(f'we have {len(self.centroids)} persons in the images as follow:')
+        for idx in range(len(self.centroids)):
+            print(f'for person {idx}:')
+            for j in range(len((self.new_centroids_data[idx]))):
+                print(self.images_address[self.new_centroids_data[idx][j]])
 
     def draw_results(self):
-        fig = plt.figure()
         rows = len(self.centroids)
         print(self.get_max_class())
         columns = self.get_max_class()
-
-        f, axarr = plt.subplots(rows,columns)
-        for idx in range (rows):
-            for j in range (len((self.new_centroids_data[idx]))):
+        plt.axis('off')
+        f, axarr = plt.subplots(rows, columns)
+        for idx in range(rows):
+            for j in range(len((self.new_centroids_data[idx]))):
                 img = cv2.imread(self.images_address[self.new_centroids_data[idx][j]])
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 axarr[idx, j].imshow(img)
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -128,7 +115,3 @@ if __name__ == "__main__":
     CF.fit()
     CF.show_results()
     CF.draw_results()
-
-
-
-    
